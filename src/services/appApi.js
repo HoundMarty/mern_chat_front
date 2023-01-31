9cb50c94d8135ceb2d5884dfc1398f53c00f7fd8
Márty <986532741@seznam.cz>
2023-01-31 20:05:57 +0100

    render

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const appApi = createApi({
     reducerPath: 'appApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5001'
+        baseUrl: ['http://localhost:5001', "https://pwaChat.onrender.com"]
     }),
     endpoints: (builder) => ({
         // user create
