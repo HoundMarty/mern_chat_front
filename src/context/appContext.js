1554f16cc0bc0b8359257212d0c47e88d0212413
Márty <986532741@seznam.cz>
2023-01-31 20:39:49 +0100

    render

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 import React from 'react';
-const SOCKET_URL = "http://localhost:5001";
+//http://localhost:5001
+const SOCKET_URL = "https://pwachat-api.onrender.com:5001";
 
 export const socket = io(SOCKET_URL);
 export const AppContext = React.createContext();
